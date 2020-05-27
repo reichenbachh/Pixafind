092a0c7ea425292be078a19facaf0e1f39555a9f
George Andong <Georgeansong7@gmail.com>
2020-05-27 15:14:27 +0000

    created image details component,fixed await request bug

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Picture finder web app built with react.
 This app makes async calls to the unsplash API based on user queries.
 
 Uses react hooks to mimic react lifecycle methods and the context api for state management.
+
+Both custom css and materialise css were used
--- src/components/Result/ImageDetails.js
+++ src/components/Result/ImageDetails.js
@@ -13,14 +13,90 @@ const ImageDetails = ({ match }) => {
     // eslint-disable-next-line
   }, []);
 
-  if (loading && imageInfo === null) return <Preloader />;
+  if (loading || imageInfo === null) return <Preloader />;
   else if (imageInfo) {
     return (
       <Fragment>
-        <p>{imageInfo.alt_description}</p>
-        <p>{imageInfo.likes}</p>
-        <p>{imageInfo.user.bio}</p>
-        <img src={imageInfo.urls.small} alt='' />
+        <div className='imageDetails-wrapper'>
+          <div className='row'>
+            <div className='full-image col s9'>
+              <img src={imageInfo.urls.regular} className='full-image' alt='' />
+            </div>
+            <div className='image-details col s3'>
+              <div className='row'>
+                <div className='profile'>
+                  <div>
+                    <img
+                      src={imageInfo.user.profile_image.medium}
+                      className='circle'
+                      alt=''
+                    />
+                  </div>
+                  <div>
+                    <p className='user-name'>{imageInfo.user.username}</p>
+                  </div>
+                </div>
+                <div className='bio'>
+                  {imageInfo.user.bio && <p>Bio:{imageInfo.user.bio}</p>}
+                </div>
+                <div className='social'>
+                  <div className='insta'>
+                    {imageInfo.user.instagram_username && (
+                      <p>
+                        <i className='fab fa-instagram'></i>
+                        {imageInfo.user.instagram_username}
+                      </p>
+                    )}
+                  </div>
+                  <div className='twitter'>
+                    {imageInfo.user.twitter_username && (
+                      <p>
+                        <i className='fab fa-twitter'></i>
+                        {imageInfo.user.twitter_username}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                {imageInfo.user.portfolio_url && (
+                  <div className='portfolio'>
+                    <a
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      href={imageInfo.user.portfolio_url}
+                    >
+                      <p>Visit Artist's Portfolio?</p>
+                    </a>
+                  </div>
+                )}
+                <div className='image-description'>
+                  <p>Description:{imageInfo.alt_description}</p>
+                </div>
+                <div className='stats'>
+                  <div>
+                    <p>
+                      <i className='fas fa-eye'></i>
+                      {imageInfo.views}
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      <i className='fas fa-heart'></i>
+                      {imageInfo.likes}
+                    </p>
+                  </div>
+                </div>
+                <div className='location'>
+                  {imageInfo.location.city && (
+                    <p>
+                      <i class='fas fa-map-marker-alt'></i>
+                      {imageInfo.location.city}
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
